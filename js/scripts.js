9fd426070d279281cf02827251b52df5c5d8ff3c
Ruslana Yakymiv <ruslanayakymiv@gmail.com>
2024-05-15 21:00:21 +0300

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	CSS/test.css
    #	CSS/test.css.map
    #	CSS/test.scss
    #	index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,37 @@
 $('.owl-carousel').owlCarousel({
     autoplay:true,
-    autoplayTimeout: 3000,
-    autoplaySpeed: 1000,
+    autoplayTimeout: 1000,
     loop:true,
+    nav: true,
     responsive:{
         0:{
             items:1
+        },
+        480:{
+            items:2
+        },
+        768:{
+            items:3
         },
         1100:{
             items:4
         }
     }
 });
+$( function() {
+    $( "#slider-range" ).slider({
+        range: true,
+        min: 39,
+        max: 1230,
+        values: [ 39, 1230 ],
+        slide: function( event, ui ) {
+            $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+        }
+    });
+    $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
+        " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+} );
+$( function() {
+    $( "#accordion" ).accordion();
+} );
+
